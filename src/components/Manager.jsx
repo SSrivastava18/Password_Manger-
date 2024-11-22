9eddd475d7f754ae603068074d46bdd4631d3437
SSrivastava18 <saurabh18srivastava1@gamil.com>
2024-11-22 19:53:40 +0530

    styling in manager and navbar

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -4,10 +4,11 @@ const Manager = () => {
     return (
         <div>
         <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
-        <div className='container mx-auto'>
-            
-            <div className='text-black flex flex-col p-4'>
-                <input type="text" name='' id='' />
+        <div className=' my-container bg-slate-50'>
+        <h1>Password-King</h1>
+        <p>Your own Password manager</p>
+            <div className='text-black flex flex-col p-4 '>
+                <input className='rounded-full' type="text" name='' id='' />
                 <div className='flex'>
                     <input type="text" />
                     <input type="text" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
 
-    <nav className='bg-purple-200 flex justify-around items-center px-4 h-14'>
-        <div className='logo font-bold'>Password-Manger</div>
+    <nav className='bg-slate-800 text-white'>
+      <div className='my-container flex justify-around items-center px- py-5 h-14'>
+        <div className='logo font-bold text-2xl'>
+          <span className='text-green-700'> &lt;</span>
+          Password-
+          <span className='text-green-700'>King/ &gt;</span>
+          </div>
         <ul>
             <li className='flex gap-4'> 
                 <a className='hover:font-bold' href="/">Home</a>
@@ -12,6 +17,7 @@ const Navbar = () => {
                 <a className='hover:font-bold' href="">Contact</a>
             </li>
         </ul>
+        </div>
     </nav>
   )
 }
