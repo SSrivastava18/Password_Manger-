fcab0dea2161203c5f9b2217f1a6d2bf7c896243
SSrivastava18 <saurabh18srivastava1@gamil.com>
2024-11-25 09:19:21 +0530

    files changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import g from "../Images/g.png"
 
 const Navbar = () => {
   return (
@@ -10,13 +11,17 @@ const Navbar = () => {
           Password-
           <span className='text-green-700'>King/ &gt;</span>
           </div>
-        <ul>
+        {/* <ul>
             <li className='flex gap-4'> 
                 <a className='hover:font-bold' href="/">Home</a>
                 <a className='hover:font-bold' href="#">About</a>
                 <a className='hover:font-bold' href="">Contact</a>
             </li>
-        </ul>
+        </ul> */}
+        <button className='text-white bg-green-500 my-5 rounded-full flex gap-1 justify-between items-center'>
+          <img className='invert w-10 p-1 rounded-full  ' src={g} alt="git" />
+          <span className='font-bold px-1'>Github</span>
+        </button>
         </div>
     </nav>
   )
